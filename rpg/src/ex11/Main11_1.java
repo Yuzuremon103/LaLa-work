811169199efd35cd2ddc683d757aeb39fce7cafe
Yuzuremon103 <qiantianboxi@gmail.com>
2022-06-02 16:25:56 +0900

    変更しました

--- rpg/src/ex11/Main11_1.java
+++ rpg/src/ex11/Main11_1.java
@@ -5,7 +5,7 @@ public class Main11_1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		Book book = new Book("ゲド戦記", 720, "green", 4.4, "978-4-00-114588-5");
+		Book book = new Book("ゲド戦記", 720, "green", 4.5, "978-4-00-114588-5");
 			System.out.println(book.getName());
 			System.out.println(book.price);
 			System.out.println(book.color);
